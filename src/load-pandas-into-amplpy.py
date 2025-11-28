facbd8b9e39fa174fe62b2cfe0b1637a99d65116
Brady Lamson <lamsonbrady@gmail.com>
2025-11-27 20:13:45 -0700

    forgot to delete some garbageeeeeee

--- src/load-pandas-into-amplpy.py
+++ src/load-pandas-into-amplpy.py
@@ -5,18 +5,9 @@ import pandas as pd
 ampl = AMPL()
 
 df = pd.read_csv("data/mtcars.csv")
-# print(df.head())
-
 df = df.set_index('model')
 adf = AMPLDataFrame.from_pandas(df)
-# print()
 
 ampl.read("models/mtcars.mod")
 ampl.set_data(adf, "MODELS")
 ampl.eval("display mpg;")
-
-# ampl.eval("set CAR;")
-
-# ampl.eval("param mpg{CAR};")
-
-# predictors = [c for c in df.columns if c not in ["model", "mpg"]]
